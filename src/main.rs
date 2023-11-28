154a84861bec35c8af5ad5bfb312888d50b97c72
Pyrode <pyrode.01@gmail.com>
2023-11-28 20:10:41 +0530

    concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpStream, TcpListener};
 use std::io::{Write, Read};
 
+
 fn handle_tcp_stream(mut stream: TcpStream) {
     loop {
         let mut buf = [0; 512];
@@ -15,19 +16,21 @@ fn handle_tcp_stream(mut stream: TcpStream) {
     }
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                handle_tcp_stream(_stream); 
+        tokio::spawn(async move {
+            match stream {
+                Ok(_stream) => {
+                    println!("accepted new connection");
+                    handle_tcp_stream(_stream); 
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
